bea45898204925fbedebd2c31d8bed9a25f36b08
abhi <abhijit.thakker@hingehealth.com>
2023-05-17 12:17:41 -0500

    Add node button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import ReactFlow, {
   MiniMap,
   Controls,
@@ -10,20 +10,34 @@ import ReactFlow, {
 
 import 'reactflow/dist/style.css';
 
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+const initialNodes = [];
+const initialEdges = [];
 
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [xValue, setX] = useState(0);
 
   const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
 
+  const addNode = useCallback(() => {
+    setX(xValue + 200);
+    setNodes((els) => {
+      console.log(els);
+      return [
+        ...els,
+        {
+          id: `state-${Math.random()}`,
+          position: { x: xValue, y: 100 },
+          data: { label: "yo" }
+        }
+      ];
+    });
+  }, [xValue]);
+
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div style={{ width: '80vw', height: '100vh' }}>
       <ReactFlow
         nodes={nodes}
         edges={edges}
@@ -36,5 +50,10 @@ export default function App() {
         <Background variant="dots" gap={12} size={1} />
       </ReactFlow>
     </div>
+      <div style={{ width: '20vw', height: '100vh', padding: '20px' }}>
+        <button onClick={addNode}>Add node</button>
+      </div>
+    </div>
+    
   );
 }
